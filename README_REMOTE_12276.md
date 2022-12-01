5e15dca04010a5be4f028b6ee96f5e0aa11aed3d
hyesu <skxldzm123@naver.com>
2022-12-02 01:00:02 +0900

    2.7

--- README_REMOTE_12276.md
+++ README_REMOTE_12276.md
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# SuperChat
-=======
 #Noom
 
 Zoom Clone using NodeJS, WebRTC and Websockets.
->>>>>>> 1460b19 (2.7)
---
+++ src/server.js
@@ -0,0 +1,65 @@
+import http from "http";
+import express from "express";
+import SocketIo from "socket.io";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", `${__dirname}/views`);
+app.use("/public", express.static(`${__dirname}/public`));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIo(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anonymous";
+  socket.onAny((event) => console.log(`Socket Event: ${event}`));
+
+  socket.on("enter_room", (roomName, nickname, done) => {
+    socket["nickname"] = nickname;
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome", socket.nickname);
+    done();
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
+  });
+
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+});
+
+/*
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+}); */
+
+const handleListen = () => console.log(`Listening on http//localhost:3000`);
+httpServer.listen(3000, handleListen);
